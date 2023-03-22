b914c0289e24841c2c376525b31da99bfe35ae7f
Student <Student@5CD3019B2M.control.ad.sit.kmutt.ac.th>
2023-03-22 10:51:29 +0700

    one command

--- INT103.java
+++ INT103.java
@@ -14,7 +14,7 @@ public class INT103 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world");
     }
     
 }
